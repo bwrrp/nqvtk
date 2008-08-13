9671898f71dbe2e9de417a9a347191d6e5568cb4
Stef Busking <stef@scivis.net>
2008-08-13 11:58:00 +0000

    Set attribute to 0 if the VBOMesh does not know about it.

--- Rendering/VBOMesh.h
+++ Rendering/VBOMesh.h
@@ -95,7 +95,7 @@ namespace NQVTK
 			pointers.clear();
 		}
 
-		void SetupAttributes(const std::vector<GLAttribute> attributes)
+		void SetupAttributes(const std::vector<GLAttribute> requiredAttribs)
 		{
 			// Clear all attributes
 			for (attribMap::iterator it = this->attribs.begin(); 
@@ -104,8 +104,8 @@ namespace NQVTK
 				it->second->index = -1;
 			}
 			// Set attributes used by the program
-			for (std::vector<GLAttribute>::const_iterator it = attributes.begin();
-				it != attributes.end(); ++it)
+			for (std::vector<GLAttribute>::const_iterator it = requiredAttribs.begin();
+				it != requiredAttribs.end(); ++it)
 			{
 				if (it->index >= 0)
 				{
@@ -116,8 +116,56 @@ namespace NQVTK
 					}
 					else
 					{
-						qDebug("Warning! Required attribute '%s' not available!", 
-							it->name.c_str());
+						//qDebug("Warning! Required attribute '%s' not available!", 
+						//	it->name.c_str());
+
+						// Set a default value for the attribute
+						// It seems we can ignore the size, as array elements are listed 
+						// separately in requiredAttribs.
+						// Matrices seem to be listed once; rows need to be set separately
+						// TODO: type for mat4 is returned as vec4, how to distinguish?
+						std::vector<float> zeroes;
+						zeroes.resize(4 * 4);
+						switch (it->type)
+						{
+							case GL_FLOAT:
+								glVertexAttrib1fv(it->index, &zeroes[0]);
+								break;
+
+							case GL_FLOAT_MAT4x2:
+								glVertexAttrib2fv(it->index + 3, &zeroes[0]);
+							case GL_FLOAT_MAT3x2:
+								glVertexAttrib2fv(it->index + 2, &zeroes[0]);
+							case GL_FLOAT_MAT2:
+								glVertexAttrib2fv(it->index + 1, &zeroes[0]);
+							case GL_FLOAT_VEC2:
+								glVertexAttrib2fv(it->index, &zeroes[0]);
+								break;
+
+							case GL_FLOAT_MAT4x3:
+								glVertexAttrib3fv(it->index + 3, &zeroes[0]);
+							case GL_FLOAT_MAT3:
+								glVertexAttrib3fv(it->index + 2, &zeroes[0]);
+							case GL_FLOAT_MAT2x3:
+								glVertexAttrib3fv(it->index + 1, &zeroes[0]);
+							case GL_FLOAT_VEC3:
+								glVertexAttrib3fv(it->index, &zeroes[0]);
+								break;
+
+							case GL_FLOAT_MAT4:
+								glVertexAttrib4fv(it->index + 3, &zeroes[0]);
+							case GL_FLOAT_MAT3x4:
+								glVertexAttrib4fv(it->index + 2, &zeroes[0]);
+							case GL_FLOAT_MAT2x4:
+								glVertexAttrib4fv(it->index + 1, &zeroes[0]);
+							case GL_FLOAT_VEC4:
+								glVertexAttrib4fv(it->index, &zeroes[0]);
+								break;
+
+							default:
+								qDebug("Error! Attribute %s has unsupported type %d.", 
+									it->name.c_str(), it->type);
+						}
 					}
 				}
 			}
