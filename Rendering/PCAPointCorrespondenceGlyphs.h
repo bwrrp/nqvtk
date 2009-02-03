09a9d47db9035228fbe88c4c9ba61b95e95adb23
Stef Busking <stef@scivis.net>
2009-02-03 20:47:24 +0000

    Implemented PCAPointCorrespondenceGlyphs loading and drawing.

--- Rendering/PCAPointCorrespondenceGlyphs.h
+++ Rendering/PCAPointCorrespondenceGlyphs.h
@@ -2,7 +2,12 @@
 
 #include "VBOMesh.h"
 
+#include "PCAParamSet.h"
+
 #include "GLBlaat/GLBuffer.h"
+
+#include <string>
+#include <sstream>
 
 namespace NQVTK
 {
@@ -14,45 +19,112 @@ namespace NQVTK
 		PCAPointCorrespondenceGlyphs(VBOMesh *obj1, VBOMesh *obj2)
 			: obj1(obj1), obj2(obj2)
 		{
-			// Extract points from the objects
-			AttributeSet *set1 = obj1->GetAttributeSet("gl_Vertex");
-			GLBuffer *vbo1 = set1->GetBuffer();
-			AttributeSet *set2 = obj2->GetAttributeSet("gl_Vertex");
-			GLBuffer *vbo2 = set2->GetBuffer();
+			// Merge points
+			AttributeSet *points = MergeSets("gl_Vertex");
+			points->UseAsVertices();
+			AddAttributeSet("gl_Vertex", points);
 
-			// TODO: add support for other data types
-			assert(set1->GetValueType() == GL_DOUBLE);
-			assert(set2->GetValueType() == GL_DOUBLE);
+			// Now copy the eigenvectors
+			int nummodes1 = PCAParamSet::GetNumEigenModes(obj1);
+			int nummodes2 = PCAParamSet::GetNumEigenModes(obj2);
+			assert(nummodes1 == nummodes2);
 
-			assert(set1->GetNumberOfComponents() == 3);
-			assert(set2->GetNumberOfComponents() == 3);
-
-			// We can only map a single VBO at a time, so first copy to main memory
-			int numPoints = set1->GetNumberOfTuples();
-			assert(set2->GetNumberOfTuples() == numPoints);
-			// Copy points from object 1
-			std::vector<Vector3> points1;
-			points1.resize(numPoints);
-			double *data = static_cast<double*>(vbo1->Map(GL_READ_ONLY));
-			for (int i = 0; i < numPoints; ++i)
+			for (int i = 0; i < nummodes1; ++i)
 			{
-				points1[i] = Vector3 p(data[i * 3], data[i * 3 + 1], data[i * 3 + 2]);
+				std::ostringstream name;
+				name << "eigvecs[" << i << "]";
+				AttributeSet *vecset = MergeSets(name.str());
+				AddAttributeSet(name.str(), vecset, true);
 			}
-			vbo1->Unmap();
-			// TODO: also copy vbo2, and the eigenvectors!
 
-			// TODO: create merged VBOs for points and eigenvectors
-			
-			// TODO: create index buffer for glyph lines
+			// Create index buffer for glyph lines
+			numLines = obj1->GetAttributeSet("gl_Vertex")->GetNumberOfTuples();
+			lineIndices = GLBuffer::New();
+			lineIndices->BindAsIndexData();
+			lineIndices->SetData(numLines * 2 * sizeof(GLuint), 0, GL_STATIC_DRAW);
+			unsigned int *indices = reinterpret_cast<unsigned int*>(lineIndices->Map(GL_WRITE_ONLY));
+			// Create indices
+			for (int i = 0; i < numLines; ++i)
+			{
+				indices[i * 2] = i;
+				indices[i * 2 + 1] = i + numLines;
+			}
+			lineIndices->Unmap();
+			lineIndices->Unbind();
 		}
 
 		virtual void Draw() const
 		{
-			// TODO: draw glyphs
+			// Color
+			glColor4d(color.x, color.y, color.z, opacity);
+
+			// Enter object coordinates
+			PushTransforms();
+
+			// Setup vbo and pointers
+			BindAttributes();
+
+			glDisable(GL_LIGHTING);
+			glLineWidth(2.0);
+
+			lineIndices->BindAsIndexData();
+			glDrawElements(GL_LINES, numLines * 2, 
+				GL_UNSIGNED_INT, BUFFER_OFFSET(0));
+			lineIndices->Unbind();
+
+			// Unset vbo render state
+			UnbindAttributes();
+
+			// Restore world coordinates
+			PopTransforms();
 		}
 
 	protected:
 		VBOMesh *obj1;
 		VBOMesh *obj2;
+
+		int numLines;
+		GLBuffer *lineIndices;
+
+		void GetSetData(AttributeSet *set, float* data)
+		{
+			// TODO: add support for other data types
+			assert(set->GetValueType() == GL_FLOAT);
+
+			int numTuples = set->GetNumberOfTuples();
+			int numComps = set->GetNumberOfComponents();
+
+			float *mapped = reinterpret_cast<float*>(set->GetBuffer()->Map(GL_READ_ONLY));
+			memcpy(data, mapped, numTuples * numComps * sizeof(float));
+			set->GetBuffer()->Unmap();
+			set->GetBuffer()->Unbind();
+		}
+
+		AttributeSet *MergeSets(const std::string &name)
+		{
+			// Extract points from the objects
+			AttributeSet *set1 = obj1->GetAttributeSet(name);
+			AttributeSet *set2 = obj2->GetAttributeSet(name);
+
+			// Make sure the objects have the same number of values
+			int numValues1 = set1->GetNumberOfTuples() * set1->GetNumberOfComponents();
+			int numValues2 = set2->GetNumberOfTuples() * set2->GetNumberOfComponents();
+			assert(numValues1 == numValues2);
+
+			// We can only map a single VBO at a time, so first copy to main memory
+
+			// Copy points from each object
+			float *data = new float[numValues1 + numValues2];
+			GetSetData(set1, data);
+			GetSetData(set2, data + numValues1);
+
+			// Create a new VBO
+			AttributeSet *combined = new AttributeSet(GL_FLOAT, set1->GetNumberOfComponents());
+			combined->SetData(set1->GetNumberOfTuples() + set2->GetNumberOfTuples(), data);
+
+			delete [] data;
+
+			return combined;
+		}
 	};
 }
