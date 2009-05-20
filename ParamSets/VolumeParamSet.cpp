69338dafb4146320729cfb3a17ef081657d4f9dd
Stef Busking <stef@scivis.net>
2009-05-20 11:41:52 +0000

    Fixed crash in SetupProgramArrays when the volume is null.

--- ParamSets/VolumeParamSet.cpp
+++ ParamSets/VolumeParamSet.cpp
@@ -52,32 +52,35 @@ namespace NQVTK
 	void VolumeParamSet::SetupProgramArrays(
 		GLProgram *program, int objectId)
 	{
-		program->SetUniform1f(
-			GetArrayName("volumeDataShift", objectId), 
-			static_cast<float>(volume->GetDataShift()));
-		program->SetUniform1f(
-			GetArrayName("volumeDataScale", objectId), 
-			static_cast<float>(volume->GetDataScale()));
-		NQVTK::Vector3 origin = volume->GetOrigin();
-		program->SetUniform3f(
-			GetArrayName("volumeOrigin", objectId), 
-			static_cast<float>(origin.x), 
-			static_cast<float>(origin.y), 
-			static_cast<float>(origin.z));
-		NQVTK::Vector3 size = volume->GetOriginalSize();
-		program->SetUniform3f(
-			GetArrayName("volumeSize", objectId), 
-			static_cast<float>(size.x), 
-			static_cast<float>(size.y), 
-			static_cast<float>(size.z));
-		program->SetUniform3f(
-			GetArrayName("volumeSpacing", objectId), 
-			static_cast<float>(
-				size.x / static_cast<double>(volume->GetWidth() - 1)), 
-			static_cast<float>(
-				size.y / static_cast<double>(volume->GetHeight() - 1)), 
-			static_cast<float>(
-				size.z / static_cast<double>(volume->GetDepth() - 1)));
+		if (volume)
+		{
+			program->SetUniform1f(
+				GetArrayName("volumeDataShift", objectId), 
+				static_cast<float>(volume->GetDataShift()));
+			program->SetUniform1f(
+				GetArrayName("volumeDataScale", objectId), 
+				static_cast<float>(volume->GetDataScale()));
+			NQVTK::Vector3 origin = volume->GetOrigin();
+			program->SetUniform3f(
+				GetArrayName("volumeOrigin", objectId), 
+				static_cast<float>(origin.x), 
+				static_cast<float>(origin.y), 
+				static_cast<float>(origin.z));
+			NQVTK::Vector3 size = volume->GetOriginalSize();
+			program->SetUniform3f(
+				GetArrayName("volumeSize", objectId), 
+				static_cast<float>(size.x), 
+				static_cast<float>(size.y), 
+				static_cast<float>(size.z));
+			program->SetUniform3f(
+				GetArrayName("volumeSpacing", objectId), 
+				static_cast<float>(
+					size.x / static_cast<double>(volume->GetWidth() - 1)), 
+				static_cast<float>(
+					size.y / static_cast<double>(volume->GetHeight() - 1)), 
+				static_cast<float>(
+					size.z / static_cast<double>(volume->GetDepth() - 1)));
+		}
 	}
 
 	// ------------------------------------------------------------------------
